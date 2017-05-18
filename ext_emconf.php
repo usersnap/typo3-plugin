10f523a5b21d92b893cb9e48e2ba107b04b877ab
Gerald Aistleitner <g.aistleitner@siwa.at>
2017-05-18 11:40:06 +0200

    first commit

---
+++ ext_emconf.php
@@ -0,0 +1,44 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "usersnap".
+#
+# Auto generated 17-08-2015 15:30
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Usersnap',
+	'description' => '',
+	'category' => 'plugin',
+	'shy' => 0,
+	'version' => '0.1.2',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'TYPO3_version' => '6.2.0-8.7.99',
+  	'constraints' => array(
+    		'depends' => array(
+      			'typo3' => '6.2.0-8.7.99',
+    		),
+  	),
+	'PHP_version' => '',
+	'module' => '',
+	'state' => 'beta',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Jochen Landvoigt & Gerald Aistleitner',
+	'author_email' => 'j.landvoigt@siwa.at',
+	'author_company' => 'SIWA Online GmbH',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+);
+
+?>
